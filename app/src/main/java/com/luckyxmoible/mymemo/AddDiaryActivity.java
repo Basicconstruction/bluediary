15d78468723209d8d344e47c3086af3de0422349
wcx <2024240041@henu.edu.cn>
2021-07-27 19:08:23 +0800

    转向LinearLayoutCompat，而不是过于依赖ConstraintLayout. 前者似乎更适合来构造页面。 android:layout_alignParentLeft, TextView android:gravity=“center”,也促进了布局。 更新:自定义了应用栏（未完全完成）

--- app/src/main/java/com/luckyxmoible/mymemo/AddDiaryActivity.java
+++ app/src/main/java/com/luckyxmoible/mymemo/AddDiaryActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -25,8 +26,10 @@ public class AddDiaryActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_diary_activity);
-        String title = "写日记";
-        this.getSupportActionBar().setTitle(title);
+        //String title = "写日记";
+        //this.getSupportActionBar().setTitle(title);
+        Toolbar toolbar = findViewById(R.id.add_toolbar);
+        setSupportActionBar(toolbar);
         TextView time_tv = (TextView)findViewById(R.id.time);
         time_tv.setText(Diary.getDateTime());
         Button button = (Button)findViewById(R.id.submit_button);
