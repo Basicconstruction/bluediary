35d1d1a19432bf5fdbc5416ffb8b24238264d495
wcx <2024240041@henu.edu.cn>
2021-07-30 10:06:56 +0800

    添加ImageView布局，有待优化

--- app/src/main/java/com/luckyxmoible/mymemo/AddDiaryActivity.java
+++ app/src/main/java/com/luckyxmoible/mymemo/AddDiaryActivity.java
@@ -2,8 +2,10 @@ package com.luckyxmoible.mymemo;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,28 +14,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class AddDiaryActivity extends AppCompatActivity {
     private final int MENU_EMPTY = 3;
+    Uri imageUri;
+    TextView time_tv;
+    Toolbar toolbar;
+    ImageButton push_add;
+    ImageButton back_add;
+    ImageButton select_image;
+    ImageView show_image;
+    private static final int PICK_IMAGE = 100;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_diary_activity);
         //String title = "写日记";
         //this.getSupportActionBar().setTitle(title);
-        Toolbar toolbar = findViewById(R.id.add_toolbar);
+        toolbar = findViewById(R.id.add_toolbar);
         setSupportActionBar(toolbar);
-        TextView time_tv = (TextView)findViewById(R.id.time);
+        time_tv = (TextView)findViewById(R.id.time);
         time_tv.setText(Diary.getDateTime());
-        ImageButton push_add = (ImageButton)findViewById(R.id.push_add);
+        push_add = (ImageButton)findViewById(R.id.push_add);
         push_add.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -57,11 +69,19 @@ public class AddDiaryActivity extends AppCompatActivity {
                 startActivity(new Intent(AddDiaryActivity.this,MainActivity.class));
             }
         });
-        ImageButton back_add = (ImageButton)findViewById(R.id.back_add);
+        back_add = (ImageButton)findViewById(R.id.back_add);
         back_add.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 startActivity(new Intent(view.getContext(),MainActivity.class));
+            }
+        });
+        select_image = (ImageButton)findViewById(R.id.image_select);
+        show_image = (ImageView)findViewById(R.id.image_show);
+        select_image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openGalley();
             }
         });
     }
@@ -75,5 +95,16 @@ public class AddDiaryActivity extends AppCompatActivity {
     public boolean empty_diary_call(MenuItem item){
         return empty_content();
     }
-
+    public void openGalley(){
+        Intent galley = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galley,PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int resquestCode,int resultCode,Intent data){
+        super.onActivityResult(resquestCode,resultCode,data);
+        if(resultCode==RESULT_OK&&resquestCode==PICK_IMAGE){
+            imageUri = data.getData();
+            show_image.setImageURI(imageUri);
+        }
+    }
 }
---
+++ app/src/main/java/com/luckyxmoible/mymemo/ImageListFragment.java
@@ -0,0 +1,23 @@
+package com.luckyxmoible.mymemo;
+
+import androidx.fragment.app.Fragment;
+
+public class ImageListFragment extends Fragment {
+    /*
+    *
+    *
+    <fragment android:name="com.luckyxmoible.mymemo.ImageListFragment"
+        android:id="@+id/recyclerView"
+        android:layout_width="0dp"
+        android:layout_height="0dp"
+        app:layout_constraintBottom_toTopOf="@id/delete_button"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        android:paddingTop="5dp"
+        android:clipToPadding="false"
+        />
+
+    *
+    * */
+}
